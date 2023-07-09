2062cb1e9c8f216728e72532e56e3334619e8502
dohlee <dohlee.bioinfo@gmail.com>
2023-07-09 22:33:38 +0900

    prepare class templates

--- diffab_pytorch/diffab_pytorch.py
+++ diffab_pytorch/diffab_pytorch.py
@@ -4,6 +4,54 @@ import torch.nn.functional as F
 
 import pytorch_lightning as pl
 
+class SingleAminoAcidEmbeddingMLP(nn.Module):
+
+    def __init__(self):
+        pass
+
+    def forward(self, x):
+        pass
+
+class PairwiseEmbeddingMLP(nn.Module):
+
+    def __init__(self):
+        pass
+
+    def forward(self, x):
+        pass
+
+class InvariantPointAttention(nn.Module):
+
+    def __init__(self):
+        pass
+
+    def forward(self, x):
+        pass
+
+class AminoAcidDenoising(nn.Module):
+
+    def __init__(self):
+        pass
+
+    def forward(self, x):
+        pass
+
+class CaCoordinateDenoising(nn.Module):
+
+    def __init__(self):
+        pass
+
+    def forward(self, x):
+        pass
+
+class OrientationDenoising(nn.Module):
+
+    def __init__(self):
+        pass
+
+    def forward(self, x):
+        pass
+
 class DiffAb(pl.LightningModule):
 
     def __init__(self):
