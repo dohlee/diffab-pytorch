e923e11064975c8b0b4ff097b61327f639e7e9aa
dohlee <dohlee.bioinfo@gmail.com>
2023-07-10 17:52:55 +0900

    implement SO3-so3 operations

--- diffab_pytorch/so3.py
+++ diffab_pytorch/so3.py
@@ -4,6 +4,19 @@ from einops import rearrange
 from scipy.spatial.transform import Rotation
 
 
+def uniform(*size):
+    assert len(size) >= 2, "size must be at least 2-dimensional"
+    assert size[-2] == size[-1] == 3, "last two dimensions must be 3"
+
+    # sample from uniform distribution
+    n_sample = 1
+    for dim in size[:-2]:
+        n_sample *= dim
+
+    R = Rotation.random(n_sample).as_matrix().reshape(*size)
+    return torch.tensor(R).float()
+
+
 def tensor_trace(T):
     return T.diagonal(offset=0, dim1=-2, dim2=-1).sum(dim=-1)
 
@@ -18,11 +31,13 @@ def log_rotmat(R):
     R (torch.tensor): bsz, L, 3, 3
     """
     tr = tensor_trace(R)
-    theta = torch.acos((tr - 1) / 2)
+
+    cos_theta = (tr - 1) / 2
+    theta = torch.acos(cos_theta)
     theta = rearrange(theta, "b l -> b l () ()")
 
     # TODO: should handle when theta == 0
-    return theta / (2 * torch.sin(theta)) * (R - R.transpose(2, 3))
+    return theta / (2 * torch.sin(theta)) * (R - R.transpose(-1, -2))
 
 
 def skew_symmetric_mat_to_vector(S):
@@ -42,19 +57,22 @@ def exp_skew_symmetric_mat(S):
 
     S (torch.tensor): bsz, L, 3, 3
     """
+    v = skew_symmetric_mat_to_vector(S)
 
-    theta = torch.norm(S, dim=(-2, -1), keepdim=True)
-    theta = rearrange(theta, "b l () () -> b l", l=S.shape[1])
+    norm = v.norm(dim=-1)
+    norm = rearrange(norm, "b l -> b l () ()")
+
+    iden = torch.eye(3).to(S.device).expand_as(S)
+
+    R = iden + S * torch.sin(norm) / norm + S @ S * (1 - torch.cos(norm)) / norm**2
+
+    return R
 
 
-def uniform(*size):
-    assert len(size) >= 2, "size must be at least 2-dimensional"
-    assert size[-2] == size[-1] == 3, "last two dimensions must be 3"
+def scale_rot(R, k):
+    """
+    Scale a rotation matrix R by a scalar k.
 
-    # sample from uniform distribution
-    n_sample = 1
-    for dim in size[:-2]:
-        n_sample *= dim
-
-    R = Rotation.random(n_sample).as_matrix().reshape(*size)
-    return torch.tensor(R).float()
+    k (int): scalar
+    """
+    return exp_skew_symmetric_mat(k * log_rotmat(R))
--- tests/test_so3.py
+++ tests/test_so3.py
@@ -1,11 +1,16 @@
 import pytest
 import torch
 
+import numpy as np
+
+from torch import einsum
 from diffab_pytorch.so3 import (
+    uniform,
     tensor_trace,
     log_rotmat,
     skew_symmetric_mat_to_vector,
-    uniform,
+    exp_skew_symmetric_mat,
+    scale_rot,
 )
 
 
@@ -36,8 +41,49 @@ def test_skew_symmetric_mat_to_vector():
     assert v.shape == (bsz, L, 3)
 
 
+def test_exp_skew_symmetric_mat():
+    bsz, L = 32, 100
+
+    R = uniform(bsz, L, 3, 3)
+    S = log_rotmat(R)
+    R_recon = exp_skew_symmetric_mat(S)
+
+    for i in range(bsz):
+        for j in range(L):
+            tr = tensor_trace(R[i, j])
+            cos_theta = (tr - 1) / 2
+
+            # skip if theta is close to 0 or pi
+            # since the computation becomes unstable
+            if (cos_theta - 1.0).abs() < 1e-2 or (cos_theta - (-1.0)).abs() < 1e-2:
+                continue
+
+            diff = (R[i, j] - R_recon[i, j]).abs().sum()
+            assert diff < 1e-4
+
+
 def test_uniform():
     bsz, L = 32, 100
 
     R = uniform(bsz, L, 3, 3)
     assert R.shape == (bsz, L, 3, 3)
+
+    # Check that R is a rotation matrix
+    # i.e. R^T R = I
+    product = einsum("b l i j, b l j k -> b l i k", R.transpose(2, 3), R)
+    assert torch.allclose(product, torch.eye(3).expand_as(product), rtol=1e-5, atol=1e-5)
+
+
+@pytest.mark.parametrize("k", np.linspace(0.1, 1, 10))
+def test_scale_rot(k):
+    bsz, L = 32, 100
+
+    R = uniform(bsz, L, 3, 3)
+    R_scaled = scale_rot(R, k)
+
+    assert R_scaled.shape == (bsz, L, 3, 3)
+
+    # Check that R_scaled is a rotation matrix
+    # i.e. R^T R = I
+    product = einsum("b l i j, b l j k -> b l i k", R_scaled.transpose(2, 3), R_scaled)
+    assert torch.allclose(product, torch.eye(3).expand_as(product), rtol=1e-5, atol=1e-5)
